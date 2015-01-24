319bf59ee5da1dc82e139a939c3d6bbaa789c6f3
velio84 <v.golev@yahoo.com>
2015-01-24 12:31:54 +0200

    Homework 1

---
+++ _01_Defining-Classes/_04_SULS/_04_SULS/OnlineStudent.cs
@@ -0,0 +1,11 @@
+﻿namespace _04_SULS
+{
+    public class OnlineStudent : CurrentStudent
+    {
+        public OnlineStudent(string firstName, string lastName, int age, string studentNumber, decimal avrgGrade, string currentCourse)
+            : base(firstName, lastName, age, studentNumber, avrgGrade, currentCourse)
+        {
+
+        }
+    }
+}
---
+++ _01_Defining-Classes/_04_SULS/_04_SULS/SULSTest.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _04_SULS
+{
+   public class SULSTest
+    {
+        public static void Main()
+        {
+            List<Student> students = new List<Student>
+            {
+                new OnsiteStudent("Peter", "Petrov", 25, "000001", 3.4m, "OOP"),
+                new OnsiteStudent("Anna", "Kirova", 27, "000002", 5.8m, "OOP"),
+                new OnlineStudent("Kiril", "Hristov", 30, "000006", 4.65m, "OOP"),
+                new GraduateStudent("Ivan", "Ivanov", 29, "000005", 5.17m),
+                new DropoutStudent("Ceco", "Avramov", 22, "000015", "Playing CounterStrike during lectures", 2.89m)
+            };
+
+            //sorting current students by average grade
+            var currentStudents = students
+                                    .Where(st => st is CurrentStudent)
+                                    .OrderBy(st => st.AverageGrade)
+                                    .Select(st => new
+                                        {
+                                            Name = st.FirstName + " " + st.LastName,
+                                            Age = st.Age,
+                                            ID = st.StudentNumber,
+                                            AvrGrade = st.AverageGrade
+                                        });
+
+            foreach (var st in currentStudents)
+            {
+                Console.WriteLine(st);
+            }
+
+
+            Console.WriteLine();
+
+            //printing the Reapply() method of a dropout student
+            DropoutStudent ceco = new DropoutStudent("Ceco", "Avramov", 22, "000015", "Playing CounterStrike during lectures", 2.89m);
+            ceco.Reapply();
+
+            Console.WriteLine();
+
+            //adding and deleting courses for junior and senior trainers
+            JuniorTrainer acho = new JuniorTrainer("Angel", "Georgiev", 28);
+            SeniorTrainer nakov = new SeniorTrainer("Svetlin", "Nakov", 35);
+
+            acho.CreateCourse("JS Apps");
+            acho.CreateCourse("Java Basics");
+            Console.WriteLine("Courses that Angel is training: " + String.Join(", ", acho.Courses) + "\n");
+
+            nakov.CreateCourse("OOP");
+            nakov.CreateCourse("Advanced Java");
+            Console.WriteLine("Courses that Nakov is training: " + String.Join(", ", nakov.Courses) + "\n");
+            nakov.DeleteCourse("OOP");
+            Console.WriteLine("Courses that Nakov is training: " + String.Join(", ", nakov.Courses));
+        }
+    }
+}
