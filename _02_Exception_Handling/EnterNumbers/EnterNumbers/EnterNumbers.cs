317f449f884da643fa94d65ee19db0878d8ea9a6
velio84 <v.golev@yahoo.com>
2015-01-28 01:49:20 +0200

    Homework 2-2

---
+++ _02_Exception_Handling/EnterNumbers/EnterNumbers/EnterNumbers.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EnterNumbers
+{
+    class EnterNumbers
+    {
+        static void Main(string[] args)
+        {
+            List<int> nums = new List<int>();
+            int start = 1;
+            bool programComplete = true;
+
+            while (nums.Count() < 10)
+            {
+                int num = ReadNumbers(start, 100);
+
+                if (100 - num < 10 - nums.Count())
+                {
+                    programComplete = false;
+                    break;
+                }
+                else if (num > start)
+                {
+                    nums.Add(num);
+                    start = num;
+                }
+            }
+
+            switch (programComplete)
+            {
+                case (false):
+                    Console.WriteLine();
+                    Console.WriteLine("You will not be able to enter 10 consecutive numbers in the range [1..100] with this input.\nThe program will now terminate!");
+                    Console.WriteLine();
+                    break;
+                default:
+                    Console.WriteLine();
+                    foreach (var n in nums)
+                    {
+                        Console.Write(n + " ");
+                    }
+                    Console.WriteLine();
+                    break;
+            }
+        }
+
+        public static int ReadNumbers(int start, int end)
+        {
+            int n = 1;
+
+            try
+            {
+                Console.Write("Enter an integer number: ");
+                int num = Int32.Parse(Console.ReadLine());
+                if (num > start && num < end)
+                {
+                    return n = num;
+                }
+                else if (num <= start || num >= end)
+                    throw new ArgumentOutOfRangeException();
+            }
+            catch (FormatException)
+            {
+                Console.Error.WriteLine("Invalid input! Try again with I-N-T-E-G-E-R NUMBER!!!");
+            }
+            catch (ArgumentOutOfRangeException)
+            {
+                Console.Error.WriteLine("The number must be in the range [" + (start + 1) + "..99] ");
+            }
+
+            return n;
+        }
+
+    }
+}
---
+++ _02_Exception_Handling/SquareRoot/SquareRoot/SquareRoot.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SquareRoot
+{
+    class SquareRoot
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.Write("Enter a number: ");
+                string num = Console.ReadLine();
+                int n = Int32.Parse(num);
+
+                if (n <= 0)
+                    throw new ArgumentOutOfRangeException();
+                if (n > Int32.MaxValue || n < Int32.MinValue)
+                    throw new OverflowException();
+
+                Console.WriteLine(Math.Sqrt(n));
+            }
+            catch (FormatException fe)
+            {
+                Console.Error.WriteLine("The number must be an integer " + fe.Message);
+            }
+            catch (ArgumentOutOfRangeException are)
+            {
+                Console.Error.WriteLine("The number must be > 0 " + are.Message);
+            }
+            catch (OverflowException oe)
+            {
+                Console.Error.WriteLine("The number is too big to fit in Int32 " + oe.Message);
+            }
+            finally
+            {
+                Console.WriteLine("Good bye!");
+            }
+            
+        }
+    }
+}
